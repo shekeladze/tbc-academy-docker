72f64dff0c2be77990c175f36d6a916d77cdc66c
sheke <sheke@SHOTA-PC>
2024-05-16 22:44:36 +0400

    Add project files.

---
+++ DockerProject.WebApi/Data/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+
+namespace DockerProject.WebApi.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.People.Any())
+            {
+                return; // already seeded
+            }
+
+            var personFaker = new Faker<Person>()
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName())
+                .RuleFor(p => p.DateOfBirth, f => f.Date.PastDateOnly(60, DateOnly.FromDateTime(DateTime.Now.AddYears(-18))));
+
+            var people = personFaker.Generate(100);
+
+            foreach (Person p in people)
+            {
+                context.People.Add(p);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
